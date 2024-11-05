fdb33cbdfa2a5ec975dc5beda6f91f6a09e6dc3c
Jitesh Singh <jiteshs101@gmail.com>
2024-11-05 10:57:24 +0530

    Controller Completed

---
+++ src/main/java/com/jitesh/meteorology/controllers/WeatherController.java
@@ -0,0 +1,35 @@
+package com.jitesh.meteorology.controllers;
+
+import com.jitesh.meteorology.entities.Weather;
+import com.jitesh.meteorology.services.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+@RestController
+@RequestMapping("/api/v1/weather")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    @Autowired
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/get")
+    public ResponseEntity<Weather> getWeather(
+            @RequestParam String pincode,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+
+        Weather weather = weatherService.getWeather(pincode, date);
+        System.out.println(weather);
+        return ResponseEntity.ok(weather);
+    }
+}
